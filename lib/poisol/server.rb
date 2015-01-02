692d472c2081cb86e50106bcc26f53ff07c2c149
deepak <anandhadeepak@gmail.com>
2015-01-02 14:10:26 +0530

    create log file if not exist

--- lib/poisol/server.rb
+++ lib/poisol/server.rb
@@ -2,13 +2,14 @@ module Server
   extend self
 
   def start port
+    PoisolLog.info "Starting server...  as http://localhost:#{port}"
     require 'webrick'
     @server =  WEBrick::HTTPServer.new :Port => port, :Logger => log, :AccessLog => access_log
     @port = port
     attach_shutdown
     attach_request_handling
     Thread.new{@server.start}
-    PoisolLog.info "Server Started as http://localhost:#{port}"
+    PoisolLog.info "Server Started at http://localhost:#{port}"
   end
 
   def base_url
@@ -29,6 +30,7 @@ module Server
   end
 
   def log 
+    FileUtils.mkdir_p "log" unless File.exists?("log")
     log_file = File.open 'log/poisol_webrick.log', 'a+'
     WEBrick::Log.new log_file
   end
