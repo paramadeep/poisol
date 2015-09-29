8878b57b6f1224345df29e747edc5b4d5058b4e3
Christopher Rex <chrisoffice2025@gmail.com>
2015-09-29 21:36:46 +0530

    [Chris] Changed the 'for' method to stringify the keys instead of camelize

--- spec/functional/query/query_implicit_spec.rb
+++ spec/functional/query/query_implicit_spec.rb
@@ -1,20 +1,20 @@
 describe Poisol::Stub, "#implicit query params" do
 
   it "dynamic response" do
-    Book.new.for_author("bha").has_category({"age_group"=>"11", "publisher"=>{"name"=>"oxford"}}).build()
-    response = RestClient.get "http://localhost:3030/book",{:params => {:author=>'bha',:name=>"doni"}}
+    Book.new.for_author_id("1").has_category({"age_group"=>"11", "publisher"=>{"name"=>"oxford"}}).build()
+    response = RestClient.get "http://localhost:3030/book",{:params => {:author_id=>'1',:author_name=>"doni"}}
     expect(response.body).to eq({"title"=>"independance", "category"=>{"age_group"=>"11", "genre"=>"action", "publisher"=>{"name"=>"oxford", "place"=>"erode"}}}.to_json)
   end
 
   it "default request and response" do
     Book.new.build()
-    response = RestClient.get "http://localhost:3030/book",{:params => {:author=>'bharathi',:name=>"doni"}}
+    response = RestClient.get "http://localhost:3030/book",{:params => {:author_id=>'bharathi',:author_name=>"doni"}}
     expect(response.body).to eq({"title"=>"independance", "category"=>{"age_group"=>"10", "genre"=>"action", "publisher"=>{"name"=>"summa", "place"=>"erode"}}}.to_json)
   end
 
   it "hash params for query" do
-    Book.new.for(:author=>"val").build()
-    response = RestClient.get "http://localhost:3030/book",{:params => {:author=>'val',:name=>"doni"}}
+    Book.new.for(:author_id=>"1").build()
+    response = RestClient.get "http://localhost:3030/book",{:params => {:author_id=>'1',:author_name=>"doni"}}
     expect(response.body).to eq({"title"=>"independance", "category"=>{"age_group"=>"10", "genre"=>"action", "publisher"=>{"name"=>"summa", "place"=>"erode"}}}.to_json)
   end
 
